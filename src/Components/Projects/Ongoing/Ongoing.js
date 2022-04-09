20c55c99415662edbb68f262e6a001ad04e92d99
sharifok1 <sharifok1@gmail.com>
2022-04-09 23:30:40 +0600

    added three folder

---
+++ src/Components/Projects/Ongoing/Ongoing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Ongoing = () => {
+    return (
+        <div>
+             <div>
+            <h1 className='text-dark'>Ongoing</h1>
+        </div>
+        </div>
+    );
+};
+
+export default Ongoing;
